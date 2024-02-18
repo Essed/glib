bff3bc4660d20ef0a981f2b9d5b59cef45aecb1f
Antony <vip.ar15@mail.ru>
2024-02-18 22:55:18 +0400

    ServerUpt

--- src/game/server/performer.py
+++
@@ -1,9 +0,0 @@
-
-from game.server.server import GameServer
-
-class ServerPerformer:
-    def __init__(self, server: GameServer,) -> None:
-        self.__server = server
-    
-    def perform(self)
-        self.__server.perform_action()
